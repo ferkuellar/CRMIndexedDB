5d84590713d18d76cf069967341ce0c2c787d90d
Fernando Cuellar <fercuellar@gmail.com>
2023-03-03 14:57:58 -0600

    se modificaron app y nuevo cliente

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -30,10 +30,25 @@
         const empresa = document.querySelector('#empresa').value;
 
         if(nombre === '' || email === '' || telefono === '' || empresa === '') {
-            console.error('error');
+            imprimirAlerta('Todos los campos son obligatorios', 'error');
 
-            return
+            return;
         }
+    };
+
+    function imprimirAlerta(mensaje, tipo) {
+        // crear la alerta
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center', 'border');
+
+        if(tipo === 'error') {
+            divMensaje.classList.add('bg-red-100', 'border-red-400', 'text-red-700');
+        } else {
+            divMensaje.classList.add('bg-green-100', 'border-green-400', 'text-green-700');
+        };
+        divMensaje.textContent = mensaje;
+
+        formulario.appendChild(divMensaje);
     };
 
 })();
