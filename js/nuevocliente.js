4be09cc8e8c04c31d070557f3846c9754fc90641
Fernando Cuellar <fercuellar@gmail.com>
2023-03-05 09:32:22 -0600

    Inicio de la edicion de cliente

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -43,8 +43,42 @@
         };
 
         // crear un objeto con la informacion
+
+        const cliente = {
+            nombre,
+            email,
+            telefono,
+            empresa
+        }
         
+        cliente.id = Date.now();
+        crearNuevoCliente(cliente);
     };
+
+    function crearNuevoCliente(cliente) {
+        // nuevo cliente
+        const transaction = DB.transaction(['crm'], 'readwrite');
+        const objectStore = transaction.objectStore('crm');
+
+        // console.log(objecStore);
+
+        objectStore.add(cliente);
+        
+        transaction.oncomplete = () => {
+            console.log('Cliente Agergado');
+
+            // mostrar mensaje que todo esta bien
+            imprimirAlerta('Se agergo correctamente');
+            setTimeout(() => {
+                window.location.href = 'index.html';
+            }, 3000);
+        };
+
+        transaction.onerror = () => {
+            // console.log('Hubo un error');
+            imprimirAlerta('Hubo un Error', 'error');
+        };
+    }
 
     function imprimirAlerta(mensaje, tipo) {
 
